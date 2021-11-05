7ce503de5c04767c2fb31473806a0a1666d3b83d
Fadli Ramadhan <fadlir93@gmail.com>
2021-11-05 10:06:54 +0800

    template adminLTE handling manajemen kategori

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/kategori', 'App\Http\Controllers\CategoryController', ['except' => ['create', 'show']]);
